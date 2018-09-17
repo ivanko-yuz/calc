50bdeb574b7a6e1acc0b0fbc2260b140cc0b07b3
ivan.yuzvyshyn <iyuzv@softserveinc.com>
2018-09-17 12:56:57 +0300

    add webapi project

---
+++ calc.WebApi/Controllers/CalcController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using calc.WebApi.Managers;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace calc.WebApi
+{
+    [Route("api/[controller]")]
+    public class CalcController : ControllerBase
+    {
+        CalcFacade calc;
+        public CalcController()
+        {
+            calc = new CalcFacade();
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"value {id}";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            var result = calc.Action(value);
+
+            return result;
+        }
+
+    }
+}
---
+++ calc.WebApi/Facade/CalcFacade.cs
@@ -0,0 +1,23 @@
+﻿
+namespace calc.WebApi.Managers
+{
+    public class CalcFacade
+    {
+        private ICalculator Calculator;
+        private IRPN RPN;
+        public CalcFacade()
+        {
+            RPN = new RPN();
+            Calculator = new Calculator();
+        }
+
+        public string Action(string value)
+        {
+            return Calculator.Calculate(RPN.LengyelFormaKonvertalas(value));
+        }
+
+
+
+
+    }
+}
