6b95402fe46e92849a043f4df0ff1533e56097ba
ivan.yuzvyshyn <iyuzv@softserveinc.com>
2018-09-18 12:47:21 +0300

    add GRPC

---
+++ calc.Grpc/CalcClientManager.cs
@@ -0,0 +1,29 @@
+﻿
+using Grpc.Core;
+using System.Threading.Tasks;
+using static calc.CalcService;
+
+namespace calc.Grpc
+{
+    public class CalcClientManager
+    {
+        private Channel _channel;
+        private CalcServiceClient _client;
+
+        public void Connect()
+        {
+            _channel = new Channel("localhost:1234", ChannelCredentials.Insecure);
+            _client = new CalcServiceClient(_channel);
+        }
+
+        public async Task<string> SendMessageAsync(string formula)
+        {
+            return (await _client.GetAnswerAsync(new CalcRequest() { Formula = formula })).Answer;
+        }
+
+        public async Task ShutDownAsync()
+        {
+            await _channel.ShutdownAsync();
+        }
+    }
+}
